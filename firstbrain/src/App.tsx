27a8031637ca6ea0fa0c3a304a473205dc0c1b17
matCh00 <mateusz.chalik.g@gmail.com>
2023-05-20 01:46:39 +0200

    test

--- firstbrain/src/App.tsx
+++ firstbrain/src/App.tsx
@@ -41,29 +41,29 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
+          <Route exact path="./tab1">
             <Tab1 />
           </Route>
-          <Route exact path="/tab2">
+          <Route exact path="./tab2">
             <Tab2 />
           </Route>
-          <Route path="/tab3">
+          <Route path="./tab3">
             <Tab3 />
           </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
+          <Route exact path="./">
+            <Redirect to="./tab1" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
+          <IonTabButton tab="tab1" href="./tab1">
             <IonIcon aria-hidden="true" icon={triangle} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
+          <IonTabButton tab="tab2" href="./tab2">
             <IonIcon aria-hidden="true" icon={ellipse} />
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
+          <IonTabButton tab="tab3" href="./tab3">
             <IonIcon aria-hidden="true" icon={square} />
             <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
--- firstbrain/vite.config.ts
+++ firstbrain/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://match00.github.io/FirstBrain/',
   plugins: [react()],
   test: {
     globals: true,
